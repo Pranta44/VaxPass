a584219d586fcbe5ff0b2670469299080fca4119
Teanna Pearson <45774278+TeannaP18@users.noreply.github.com>
2021-11-18 17:43:26 -0500

    created activities and added backgrounds to each

---
+++ app/src/main/java/cosc490/morgan/vaxpass/VaxRecordAdapter.java
@@ -0,0 +1,23 @@
+package cosc490.morgan.vaxpass;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+//for the recycler view on the home page
+public class VaxRecordAdapter {
+    //pass in context and list of vaccination records
+
+    //for each row, inflate layout
+
+    //bind values based on position of the element
+
+    //define view holder
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
