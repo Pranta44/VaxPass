4ec1ee2f10275a0a5b1a7f02454fbfa1e7408ee8
Teanna Pearson <45774278+TeannaP18@users.noreply.github.com>
2021-12-09 12:43:09 -0500

    bottom navigation stuff

--- app/src/main/java/cosc490/morgan/vaxpass/HomeActivity.java
+++ app/src/main/java/cosc490/morgan/vaxpass/HomeActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +13,9 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navBar);
+
+
+
 }
