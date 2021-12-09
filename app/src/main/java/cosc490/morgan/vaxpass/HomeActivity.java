4cb8a53b3a637c3e21075769f662a6db3d09b0a2
Greg Pearson <pearsong35@gmail.com>
2021-12-09 18:20:11 -0500

    initial nav bar functionality

--- app/src/main/java/cosc490/morgan/vaxpass/HomeActivity.java
+++ app/src/main/java/cosc490/morgan/vaxpass/HomeActivity.java
@@ -1,8 +1,15 @@
 package cosc490.morgan.vaxpass;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,6 +19,30 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navBar);
+
+        bottomNavigationView.setSelectedItemId(R.id.ic_home);
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_home:
+                        startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        overridePendingTransition(0,0);
+                    case R.id.ic_record:
+                        startActivity(new Intent(getApplicationContext(),VaccinationRecordActivity.class));
+                        overridePendingTransition(0,0);
+                    case R.id.ic_info:
+                        startActivity(new Intent(getApplicationContext(),CovidInfoActivity.class));
+                        overridePendingTransition(0,0);
+                }
+            }
+        });
+//        NavController navController = Navigation.findNavController(this,R.id.nav_controller_view_tag);
+//        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+
     }
 
     //BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navBar);
